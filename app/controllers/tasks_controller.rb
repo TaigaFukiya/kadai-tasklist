42e718ff703a7d5605ea2d69aa7872488e7a7a9f
tiger <ftaigasin1998@gmail.com>
2019-03-10 15:17:53 +0000

    KADAI KANSEI2

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy]
+  before_action :correct_user, only: [:destroy, :edit]
   
   def index
     @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
@@ -57,7 +57,8 @@ class TasksController < ApplicationController
   def correct_user
     @task = current_user.tasks.find_by(id: params[:id])
     unless @task
-      redirect_to root_url
+      flash[:notice] = "権限がありません"
+      redirect_to tasks_url
     end
   end
 end
