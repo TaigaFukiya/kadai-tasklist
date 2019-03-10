a21bde0bcdaca64cc40557be8a2115ee8fd7c22c
tiger <ftaigasin1998@gmail.com>
2019-03-10 12:00:21 +0000

    KADAI1

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,11 +1,13 @@
 class TasksController < ApplicationController
-  before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :require_user_logged_in
+  before_action :correct_user, only: [:destroy]
   
   def index
     @tasks = Task.order(created_at: :desc).page(params[:page]).per(10)
   end
-
+  
   def show
+    @task = Task.find(params[:id])
   end
 
   def new
@@ -13,21 +15,23 @@ class TasksController < ApplicationController
   end
 
   def create
-    @task = Task.new(task_params)
-    
+    @task = current_user.tasks.build(task_params)
     if @task.save
       flash[:success] = "Taskが正常に登録されました"
       redirect_to @task
     else
+      @tasks = current_user.tasks.order('created_at DESC').page(params[:page])
       flash.now[:danger] = "Taskが登録されませんでした"
-      render :new
+      render "new"
     end
   end
 
   def edit
+    @task = Task.find(params[:id])
   end
 
   def update
+     @task = Task.find(params[:id])
     if @task.update(task_params)
       flash[:success] = "Taskが正常に更新されました"
       redirect_to @task
@@ -45,11 +49,15 @@ class TasksController < ApplicationController
   end
   
   private
-  def set_task
-    @task = Task.find(params[:id])
-  end
   
   def task_params
     params.require(:task).permit(:content, :status)
+  end
+  
+  def correct_user
+    @task = current_user.tasks.find_by(id: params[:id])
+    unless @task
+      redirect_to root_url
+    end
   end
 end
